89464a53faba19bd24c36d57fed5f8f97227cecb
datashaman <marlinf@datashaman.com>
2019-12-06 09:07:30 +0200

    remove cruft

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -2,18 +2,6 @@ export default [
     { path: '/', redirect: '/commands' },
 
     {
-        path: '/jobs/:id',
-        name: 'job-preview',
-        component: require('./screens/jobs/preview').default,
-    },
-
-    {
-        path: '/jobs',
-        name: 'jobs',
-        component: require('./screens/jobs/index').default,
-    },
-
-    {
         path: '/commands/:id',
         name: 'command-preview',
         component: require('./screens/commands/preview').default,
@@ -23,17 +11,5 @@ export default [
         path: '/commands',
         name: 'commands',
         component: require('./screens/commands/index').default,
-    },
-
-    {
-        path: '/schedule/:id',
-        name: 'schedule-preview',
-        component: require('./screens/schedule/preview').default,
-    },
-
-    {
-        path: '/schedule',
-        name: 'schedule',
-        component: require('./screens/schedule/index').default,
     },
 ];
